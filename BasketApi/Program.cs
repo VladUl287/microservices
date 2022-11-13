74cdd026020185ff71bb7e2d7f9923e49673105e
unknown <ulyanovskiy.01@mail.ru>
2022-11-13 12:39:26 +0300

    update

--- BasketApi/Program.cs
+++ BasketApi/Program.cs
@@ -38,13 +38,15 @@ builder.Services.AddHttpClient<BookService>(config =>
     config.BaseAddress = new Uri(bookApi);
 });
 
-builder.Services.AddHostedService<SubsriberService>();
+//builder.Services.AddSingleton<IMessageBus, RabbitBus>(new RabbitBus(""));
+//builder.Services.AddHostedService<SubsriberService>();
 
 builder.Services.AddAutoMapper(config =>
 {
     config.AddProfile<MappingProfile>();
 });
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -55,6 +57,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseCors(config =>
 {
