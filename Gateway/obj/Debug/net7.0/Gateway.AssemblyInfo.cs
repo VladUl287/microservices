f7023e1313486785393fd3981f8e8da2444dbcee
unknown <ulyanovskiy.01@mail.ru>
2022-11-23 19:56:24 +0300

    ocelot swagger fixes

--- Gateway/obj/Debug/net7.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net7.0/Gateway.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2b8e9beb-a675-46c9-af3e-66cd983af54d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
